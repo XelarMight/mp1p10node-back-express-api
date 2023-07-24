e26188aca445cd267fd540c10bbec0c8b944a20d
Tendry Alex <tendry1234@gmail.com>
2023-07-24 22:26:53 +0300

    init app

--- index.js
+++ index.js
@@ -5,14 +5,25 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
+const MongoClient = require('mongodb').MongoClient
+
+const connectionString = 'mongodb+srv://repairadmin:dbTourist4321@cluster0.eeaglqg.mongodb.net/?retryWrites=true&w=majority';
+//const databaseName = 'examen_duo_mean';
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  console.log("Hello World");
-});
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
+  .then(client => {
+     console.log('Connected to Database');
+     app.get('/', (req, res) => {
+       res.setHeader("Content-Type", "application/json");
+       res.setHeader("Access-Control-Allow-Origin", "*");
+       res.send(JSON.stringify({"Hello": "Hello World"}));
+     });
 
-app.listen(3000, () => {
-  console.log('Server Started at 3000');
-});
+     app.listen(3000, () => {
+       console.log('Server Started at 3000');
+     });
+   })
+  .catch(error => console.error(error))
