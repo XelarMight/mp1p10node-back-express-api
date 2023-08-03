a7d0b86cf2c0793fbf98969a2433ecb76c4683ca
alexscriptura <tendry1234@gmail.com>
2023-08-03 18:08:55 +0300

    mise à jour

--- app/common/attraction/attraction.controller.js
+++ app/common/attraction/attraction.controller.js
@@ -1,10 +1,14 @@
+const repo_attraction = require('../../repository/attraction.repository.js');
+
 const findAllAttraction = async (req, res) => {
     // res.setHeader("Content-Type", "application/json");
     // res.setHeader("Access-Control-Allow-Origin", "*");
     // res.send(JSON.stringify({"Message": "Welcome to attraction"}));
+    const result = await repo_attraction.getAllAttractions();
     res.json({
         status: 200,
-        message: `Liste de toutes les Attractions`
+        message: `Liste de toutes les Attractions`,
+        attractions: result
     })
 }
 
